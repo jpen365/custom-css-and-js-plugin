dbd1e683f89f2a9d46fc4ad17c4f5038c6424a7d
Jon Penland <jon.penland@gmail.com>
2016-08-25 12:55:07 -0600

    Original commit. Version 1.0 for WPMU DEV Blog Post.

---
+++ custom-css-and-js.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Plugin Name:   Custom CSS and JS Plugin
+Plugin URI:    http://premium.wpmudev.org/blog/author/jonpenland
+Description:   Add a custom CSS stylesheet and JavaScript file to WordPress
+Version:       1.0
+Author:        Jon Penland
+Author URI:    http://premium.wpmudev.org/blog/author/jonpenland
+*/
+
+function custom_css_js_enqueue_scripts() {
+
+    /* enqueue the custom-style.css file */
+    wp_enqueue_style( 'custom-css', plugins_url( '/css/custom-style.css', __FILE__ ), $ver = false, $in_footer = true );
+
+    /* enqueue the custom-scripts.js file */
+    wp_enqueue_script( 'custom-js', plugins_url( '/js/custom-scripts.js', __FILE__ ), $deps = array( 'jquery' ), $ver = false, $in_footer = true );
+}
+add_action( 'wp_enqueue_scripts', 'custom_css_js_enqueue_scripts' );
