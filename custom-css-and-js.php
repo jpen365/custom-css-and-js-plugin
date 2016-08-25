fedf27edc47d6f46634764909a15f4fc00e9f4a3
Jon Penland <jon.penland@gmail.com>
2016-08-25 17:14:30 -0600

    Remove in_footer from wp_enqueue_style function

--- custom-css-and-js.php
+++ custom-css-and-js.php
@@ -11,10 +11,10 @@ Author URI:    http://premium.wpmudev.org/blog/author/jonpenland
 
 function custom_css_js_enqueue_scripts() {
 
-    /* enqueue the custom-style.css file */
-    wp_enqueue_style( 'custom-css', plugins_url( '/css/custom-style.css', __FILE__ ), $ver = false, $in_footer = true );
+  /* enqueue the custom-style.css file */
+  wp_enqueue_style( 'custom-css', plugins_url( '/css/custom-style.css', __FILE__ ), $ver = false );
 
-    /* enqueue the custom-scripts.js file */
-    wp_enqueue_script( 'custom-js', plugins_url( '/js/custom-scripts.js', __FILE__ ), $deps = array( 'jquery' ), $ver = false, $in_footer = true );
+  /* enqueue the custom-scripts.js file */
+  wp_enqueue_script( 'custom-js', plugins_url( '/js/custom-scripts.js', __FILE__ ), $deps = array( 'jquery' ), $ver = false, $in_footer = true );
 }
 add_action( 'wp_enqueue_scripts', 'custom_css_js_enqueue_scripts' );
