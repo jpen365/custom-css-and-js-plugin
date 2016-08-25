f04fa0f914feffa9b3a00549a1830c436f0b8064
Jon Penland <jon.penland@gmail.com>
2016-08-25 13:07:27 -0600

    Changed Plugin URL to GitHub URL

--- custom-css-and-js.php
+++ custom-css-and-js.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name:   Custom CSS and JS Plugin
-Plugin URI:    http://premium.wpmudev.org/blog/author/jonpenland
+Plugin URI:    https://github.com/jpen365/custom-css-and-js-plugin
 Description:   Add a custom CSS stylesheet and JavaScript file to WordPress
 Version:       1.0
 Author:        Jon Penland
